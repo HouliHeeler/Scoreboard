4cdf94e2b60756afe89ea9a8204f09a73b3f08da
HouliHeeler <luke_houlihan@hotmail.com>
2022-10-21 19:02:04 -0700

    create login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,60 @@
-export default function Login() {
-  return (
-    <div>Login</div>
-  )
+import {useState, useEffect} from 'react'
+import {FaSignInAlt} from 'react-icons/fa'
+
+function Login() {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    })
+
+    const {email, password} = formData
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    return <>
+        <section className='heading'>
+            <h1>
+                <FaSignInAlt />Login
+            </h1>
+            <p>Please Login</p>
+        </section>
+        <section className='form'>
+            <form onSubmit={onSubmit}>
+                <div className='form-group'>
+                    <input 
+                        className='form-control' 
+                        type="email" 
+                        id="email" 
+                        name="email" 
+                        value={email} 
+                        placeholder="Enter Your Email" 
+                        onChange={onChange}/>
+                </div>
+                <div className='form-group'>
+                    <input 
+                        className='form-control' 
+                        type="password" 
+                        id="password" 
+                        name="password" 
+                        value={password} 
+                        placeholder="Create Password" 
+                        onChange={onChange}/>
+                </div>
+                <div className='form-group'>
+                    <button type='submit' className='btn btn-block'>Submit</button>
+                </div>
+            </form>
+        </section>
+    </>
 }
+
+export default Login
