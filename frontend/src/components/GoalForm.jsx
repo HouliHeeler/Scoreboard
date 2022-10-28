4a77083fe7188b02c64abd8de72b9f1f735df843
HouliHeeler <luke_houlihan@hotmail.com>
2022-10-28 10:12:11 -0700

    tie nba/api to favouritePlayers functionality

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -1,13 +1,37 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import {useDispatch} from 'react-redux'
 import {createGoal} from '../features/goals/goalSlice'
-import { nbaPlayers } from '../app/constants'
+// import { nbaPlayers } from '../app/constants'
 
 function GoalForm() {
 
     const dispatch = useDispatch()
 
-    const [favouritePlayer, setFavouritePlayer] = useState('Ja Morant')
+    //Populate list of NBA Players for User to select favourite from
+
+    const [nbaPlayers, setNbaPlayers] = useState([])
+
+    function getNbaPlayers() {
+        fetch("https://data.nba.net/prod/v1/2022/players.json")
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            }
+            throw new Error("ERROR (response not ok)");
+          })
+          .then((data) => {
+            setNbaPlayers(data.league.standard);
+          })
+          .catch(() => {
+            console.log("error");
+          });
+    }
+
+    useEffect(getNbaPlayers, [])
+
+    //Select Favourite Player
+
+    const [favouritePlayer, setFavouritePlayer] = useState('Achiuwa, Precious')
 
     const onChange = (e) => {
       setFavouritePlayer(e.target.value)
@@ -17,7 +41,6 @@ function GoalForm() {
         e.preventDefault()
 
         dispatch(createGoal({favouritePlayer}))
-        console.log(favouritePlayer, typeof favouritePlayer)
     }
 
     return (
@@ -34,8 +57,8 @@ function GoalForm() {
                         placeholder="Select Favourite Player"
                         onChange={onChange}
                     >
-                        {nbaPlayers.map((option, idx) => (
-                        <option key={idx}>{option}</option>
+                        {nbaPlayers.filter(name => name.temporaryDisplayName).map((option, idx) => (
+                        <option key={idx}>{option.temporaryDisplayName}</option>
                         ))}
                     </select>
                 </div>
