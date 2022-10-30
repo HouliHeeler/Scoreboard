e256d45814c25450e4be597ecc68a08979cbe3aa
HouliHeeler <luke_houlihan@hotmail.com>
2022-10-30 09:52:40 -0700

    continue adding team color schemes to Constants

--- frontend/src/app/constants.js
+++ frontend/src/app/constants.js
@@ -47,11 +47,60 @@ export const nbaColours = [
     {CharlotteHornets: [
         {home: ["#FFFFFF", "#00788C"]},
         {away: ["#00788C", "#1d1160"]}
-    ]}
+    ]},
     {ChicagoBulls: [
         {home: ["#FFFFFF", "#CE1141"]},
         {away: ["#CE1141", "#000000"]}
-    ]}
+    ]},
+    {ClevelandCavs: [
+        {home: ["#FFFFFF", "#860038"]},
+        {away: ["#860038", "#FDBB30"]}
+    ]},
+    {DallasMavericks: [
+        {home: ["#FFFFFF", "#00538C"]},
+        {away: ["#00538C", "#FFFFFF"]}
+    ]},
+    {DenverNuggets: [
+        {home: ["#FFFFFF", "#1D428A"]},
+        {away: ["#0E2240", "#FFFFFF"]}
+    ]},
+    {DetroitPistons: [
+        {home: ["#FFFFFF", "#1D42BA"]},
+        {away: ["#1D42BA", "#C8102E"]}
+    ]},
+    {GoldenStateWarriors: [
+        {home: ["#FFFFFF", "#FDB927"]},
+        {away: ["#006BB6", "#FDB927"]}
+    ]},
+    {HoustonRockets: [
+        {home: ["#FFFFFF", "#CE1141"]},
+        {away: ["#CE1141", "#FFFFFF"]}
+    ]},
+    {IndianaPacers: [
+        {home: ["#FFFFFF", "#002D62"]},
+        {away: ["#002D62", "#FDBB30"]}
+    ]},
+    {LosAngelesClippers: [
+        {home: ["#FFFFFF", "#1D428A"]},
+        {away: ["#1D428A", "#FFFFFF"]}
+    ]},
+    {LosAngelesLakers: [
+        {home: ["#FDB927", "#552583"]},
+        {away: ["#552583", "#FDB927"]}
+    ]},
+    {MemphisGrizzlies: [
+        {home: ["#FFFFFF", "#12173F"]},
+        {away: ["#12173F", "#5D76A9"]}
+    ]},
+    {MiamiHeat: [
+        {home: ["#FFFFFF", "#98002E"]},
+        {away: ["#000000", "#98002E"]}
+    ]},
+    {MilwaukeeBucks: [
+        {home: ["#EEE1C6", "#00471B"]},
+        {away: ["#00471B", "#EEE1C6"]}
+    ]},
+
 ]
 
 //Function will return first item in chosen teams home array, adjust as needed
