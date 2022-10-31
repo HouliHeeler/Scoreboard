83c9476a6533cb4989cc616213d7fb710b01a8f0
HouliHeeler <luke_houlihan@hotmail.com>
2022-10-30 19:02:46 -0700

    complete nbaColours array

--- frontend/src/app/constants.js
+++ frontend/src/app/constants.js
@@ -100,7 +100,58 @@ export const nbaColours = [
         {home: ["#EEE1C6", "#00471B"]},
         {away: ["#00471B", "#EEE1C6"]}
     ]},
-
+    {MinnesotaTimberwolves: [
+        {home: ["#FFFFFF", "#236192"]},
+        {away: ["#236192", "#9EA2A2"]}
+    ]},
+    {NewOrleansPelicans: [
+        {home: ["#FFFFFF", "#85714D"]},
+        {away: ["#0C2340", "#85714D"]}
+    ]},
+    {NewYorkKnicks: [
+        {home: ["#FFFFFF", "#F58426"]},
+        {away: ["#006BB6", "#F58426"]}
+    ]},
+    {OklahomaCityThunder: [
+        {home: ["#FFFFFF", "#007AC1"]},
+        {away: ["#007AC1", "#F58426"]}
+    ]},
+    {OrlandoMagic: [
+        {home: ["#FFFFFF", "#0077C0"]},
+        {away: ["#0077C0", "#F58426"]}
+    ]},
+    {Philadelphia76ers: [
+        {home: ["#FFFFFF", "#006BB6"]},
+        {away: ["#ED174C", "#006BB6"]}
+    ]},
+    {PhoenixSuns: [
+        {home: ["#FFFFFF", "#E56020"]},
+        {away: ["#1D1160", "#E56020"]}
+    ]},
+    {PortlandTrailblazers: [
+        {home: ["#FFFFFF", "#E03A3E"]},
+        {away: ["#000000", "#E03A3E"]}
+    ]},
+    {SacramentoKings: [
+        {home: ["#FFFFFF", "#5A2D81"]},
+        {away: ["#5A2D81", "#FFFFFF"]}
+    ]},
+    {SanAntonioSpurs: [
+        {home: ["#C4CED4", "#000000"]},
+        {away: ["#000000", "#C4CED4"]}
+    ]},
+    {TorontoRaptors: [
+        {home: ["#FFFFFF", "#CE1141"]},
+        {away: ["#CE1141", "#000000"]}
+    ]},
+    {UtahJazz: [
+        {home: ["#FFFFFF", "#002B5C"]},
+        {away: ["#002B5C", "#F9A01B"]}
+    ]},
+    {WashingtonWizards: [
+        {home: ["#FFFFFF", "#002B5C"]},
+        {away: ["#002B5C", "#E31837"]}
+    ]},
 ]
 
 //Function will return first item in chosen teams home array, adjust as needed
