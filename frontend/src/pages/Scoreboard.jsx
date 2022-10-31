59262bd95912fbeeff8822757f3cff0c47c691c6
HouliHeeler <luke_houlihan@hotmail.com>
2022-10-31 16:05:12 -0700

    build marquee for favourite player stats

--- frontend/src/pages/Scoreboard.jsx
+++ frontend/src/pages/Scoreboard.jsx
@@ -1,5 +1,12 @@
+import Marquee from 'react-fast-marquee'
+
 function Scoreboard() {
-    return <h1>Scoreboard</h1>
+    return  <Marquee 
+                style={{border:'2px solid black'}}
+                pauseOnHover='true'>
+                <span style={{margin:'20px'}}><strong>Ja Morant</strong> - 26 Points 8 Rebounds 11 Assists 2 Steals 1 Block</span>
+                <span style={{margin: '20px'}}><strong>Luka Doncic</strong> - 41 Points 12 Rebounds 10 Assists 0 Steals 1 Block</span>
+            </Marquee>
 }
 
 export default Scoreboard
