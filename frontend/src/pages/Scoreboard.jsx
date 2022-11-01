be12a7b4475c8e65017a0e9dcec7f4bc08104a2b
HouliHeeler <luke_houlihan@hotmail.com>
2022-10-31 17:45:47 -0700

    start build of live scoreboard

--- frontend/src/pages/Scoreboard.jsx
+++ frontend/src/pages/Scoreboard.jsx
@@ -1,4 +1,6 @@
+import {useState, useEffect} from 'react'
 import Marquee from 'react-fast-marquee'
+import Spinner from '../components/Spinner'
 
 function Scoreboard() {
     //Return today's date in format required for API call
@@ -13,13 +15,48 @@ function Scoreboard() {
         return `${year}-${month}-${date}`
     }
 
+    const [scores, setScores] = useState([])
+
+    const [isLoading, setIsLoading] = useState(true)
+
+    function getScores() {
+        fetch(`https://api-nba-v1.p.rapidapi.com/games?date=${getDate}`, {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '81f0f8a38bmsh024375d5af83615p170190jsnee4713d76fa2',
+                'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+            }
+        })
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            }
+            throw new Error("ERROR (response not ok)");
+          })
+          .then((data) => {
+            setScores(data.response);
+          })
+          .then(setIsLoading(false))
+          .catch(() => {
+            console.log("error");
+          });
+    }
+
+    useEffect(getScores, [])
+
     return (
-        <Marquee 
-            style={{border:'2px solid black'}}
-            pauseOnHover='true'>
-            <span style={{margin:'20px'}}><strong>Ja Morant</strong> - 26 Points 8 Rebounds 11 Assists 2 Steals 1 Block</span>
-            <span style={{margin: '20px'}}><strong>Luka Doncic</strong> - 41 Points 12 Rebounds 10 Assists 0 Steals 1 Block</span>
-        </Marquee>
+        isLoading ? <Spinner /> :
+        <>
+            <Marquee 
+                style={{border:'2px solid black'}}
+                pauseOnHover='true'>
+                <span style={{margin:'20px'}}><strong>Ja Morant</strong> - 26 Points 8 Rebounds 11 Assists 2 Steals 1 Block</span>
+                <span style={{margin: '20px'}}><strong>Luka Doncic</strong> - 41 Points 12 Rebounds 10 Assists 0 Steals 1 Block</span>
+            </Marquee>
+            <div>
+                {scores}
+            </div>
+        </>
     ) 
 }
 
