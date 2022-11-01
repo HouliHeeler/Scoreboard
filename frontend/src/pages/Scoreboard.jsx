c17c12791830a44613f6c9b434a3fba3c41c50c9
HouliHeeler <luke_houlihan@hotmail.com>
2022-10-31 21:41:51 -0700

    work on boxscore layout

--- frontend/src/pages/Scoreboard.jsx
+++ frontend/src/pages/Scoreboard.jsx
@@ -1,56 +1,107 @@
-import {useState, useEffect} from 'react'
+// import {useState, useEffect} from 'react'
 import Marquee from 'react-fast-marquee'
-import Spinner from '../components/Spinner'
+// import {nbaColours} from '../app/constants'
+// import Spinner from '../components/Spinner'
 
 function Scoreboard() {
     //Return today's date in format required for API call
-    function getDate() {
-        let newDate = new Date()
-        let date = newDate.getDate();
-        let month = newDate.getMonth() + 1;
-        if(month < 10) {
-          month = '0' + month
-        }
-        let year = newDate.getFullYear();
-        return `${year}-${month}-${date}`
-    }
+    // function getDate() {
+    //     let newDate = new Date()
+    //     let date = newDate.getDate();
+    //     let month = newDate.getMonth() + 1;
+    //     if(month < 10) {
+    //       month = '0' + month
+    //     }
+    //     let year = newDate.getFullYear();
+    //     return `${year}-${month}-${date}`
+    // }
 
-    const currentDate = getDate()
+    // const currentDate = getDate()
+
     //Pull live scores from API using current date
 
-    const [scores, setScores] = useState([])
+    // const [scores, setScores] = useState([])
 
-    const [isLoading, setIsLoading] = useState(true)
+    // const [isLoading, setIsLoading] = useState(true)
 
-    function getScores() {
-        fetch(`https://api-nba-v1.p.rapidapi.com/games?date=${currentDate}`, {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': '81f0f8a38bmsh024375d5af83615p170190jsnee4713d76fa2',
-                'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
-            }
-        })
-          .then((response) => {
-            if (response.ok) {
-              return response.json();
-            }
-            throw new Error("ERROR (response not ok)");
-          })
-          .then((data) => {
-            setScores(data.response);
-          })
-          .then(setIsLoading(false))
-          .catch(() => {
-            console.log("error");
-          });
+    // function getScores() {
+    //     fetch(`https://api-nba-v1.p.rapidapi.com/games?date=${currentDate}`, {
+    //         method: 'GET',
+    //         headers: {
+    //             'X-RapidAPI-Key': '81f0f8a38bmsh024375d5af83615p170190jsnee4713d76fa2',
+    //             'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+    //         }
+    //     })
+    //       .then((response) => {
+    //         if (response.ok) {
+    //           return response.json();
+    //         }
+    //         throw new Error("ERROR (response not ok)");
+    //       })
+    //       .then((data) => {
+    //         setScores(data.response);
+    //       })
+    //       .then(setIsLoading(false))
+    //       .catch(() => {
+    //         console.log("error");
+    //       });
+    // }
+
+    // useEffect(getScores, [currentDate])
+
+    // console.log(scores)
+
+    const nbaScores = [
+        {
+            teamOne: 'Rockets',
+            teamTwo: 'Raptors',
+            scoreOne: 121,
+            scoreTwo: 109,
+            timeLeft: '03:08',
+            gameStatus: 'Q3',
+        },
+        {
+            teamOne: 'Sixers',
+            teamTwo: 'Lakers',
+            scoreOne: 108,
+            scoreTwo: 113,
+            timeLeft: '01:25',
+            gameStatus: 'Q4',
+        },
+        {
+            teamOne: 'Thunder',
+            teamTwo: 'Blazers',
+            scoreOne: 142,
+            scoreTwo: 114,
+            timeLeft: '00:00',
+            gameStatus: 'Final',
+        },
+    ]
+
+    // function getColours(team) {
+    //     const teamName = team.split(" ").join("")
+    //     return nbaColours[0][teamName][0].home[0]
+    // }
+
+    function scoreboards() {
+        return (
+            nbaScores.map(item => (
+                <div style={{border:'2px solid black', margin:'10px', width:'40%'}}>
+                    <img style={{width:'30px'}} alt='team logo' src="https://upload.wikimedia.org/wikipedia/fr/thumb/f/f3/Hornets_de_Charlotte_logo.svg/1200px-Hornets_de_Charlotte_logo.svg.png"></img>
+                    <span style={{margin:'10px'}}>{item.teamOne}</span>
+                    <span style={{margin:'10px'}}>{item.scoreOne}</span>
+                    <span style={{margin:'10px'}}>{item.scoreTwo}</span>
+                    <span style={{margin:'10px'}}>{item.teamTwo}</span>
+                    <img style={{width:'30px'}} alt='team logo' src="https://upload.wikimedia.org/wikipedia/fr/thumb/f/f3/Hornets_de_Charlotte_logo.svg/1200px-Hornets_de_Charlotte_logo.svg.png"></img>
+                    <br></br>
+                    <span style={{margin:'10px'}}>{item.gameStatus}{item.gameStatus !== 'Final' && `-${item.timeLeft}`}</span>
+                </div>
+            ))
+        )
     }
 
-    useEffect(getScores, [currentDate])
-
-    console.log(scores)
-
     return (
-        isLoading ? <Spinner /> :
+        // isLoading ? <Spinner /> :
         <>
             <Marquee 
                 style={{border:'2px solid black'}}
@@ -58,6 +109,7 @@ function Scoreboard() {
                 <span style={{margin:'20px'}}><strong>Ja Morant</strong> - 26 Points 8 Rebounds 11 Assists 2 Steals 1 Block</span>
                 <span style={{margin: '20px'}}><strong>Luka Doncic</strong> - 41 Points 12 Rebounds 10 Assists 0 Steals 1 Block</span>
             </Marquee>
+            {scoreboards()}
         </>
     ) 
 }
--- frontend/src/pages/Standings.jsx
+++ frontend/src/pages/Standings.jsx
@@ -3,70 +3,70 @@ import Spinner from '../components/Spinner'
 
 function Standings() {
 
-    const [isLoading, setIsLoading] = useState(true)
+    // const [isLoading, setIsLoading] = useState(true)
 
-    //Pull standings data from API
+    // //Pull standings data from API
 
-    const [standingsData, setStandingsData] = useState([])
+    // const [standingsData, setStandingsData] = useState([])
 
-    function getStandings() {
-        fetch('https://api-nba-v1.p.rapidapi.com/standings?league=standard&season=2022', {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': '81f0f8a38bmsh024375d5af83615p170190jsnee4713d76fa2',
-                'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
-            }
-        })
-          .then((response) => {
-            if (response.ok) {
-              return response.json();
-            }
-            throw new Error("ERROR (response not ok)");
-          })
-          .then((data) => {
-            setStandingsData(data.response);
-          })
-          .then(setIsLoading(false))
-          .catch(() => {
-            console.log("error");
-          });
-    }
+    // function getStandings() {
+    //     fetch('https://api-nba-v1.p.rapidapi.com/standings?league=standard&season=2022', {
+    //         method: 'GET',
+    //         headers: {
+    //             'X-RapidAPI-Key': '81f0f8a38bmsh024375d5af83615p170190jsnee4713d76fa2',
+    //             'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+    //         }
+    //     })
+    //       .then((response) => {
+    //         if (response.ok) {
+    //           return response.json();
+    //         }
+    //         throw new Error("ERROR (response not ok)");
+    //       })
+    //       .then((data) => {
+    //         setStandingsData(data.response);
+    //       })
+    //       .then(setIsLoading(false))
+    //       .catch(() => {
+    //         console.log("error");
+    //       });
+    // }
 
-    useEffect(getStandings, [])
+    // useEffect(getStandings, [])
 
-    //Set up formatting for standings layout
+    // //Set up formatting for standings layout
 
-    function getConference(conference, divOne, divTwo, divThree) {
-        return (
-            <section>
-                <h2>{conference} Conference</h2>
-                {getDivision(divOne)}
-                {getDivision(divTwo)}
-                {getDivision(divThree)}
-            </section>
-        )
-    }
+    // function getConference(conference, divOne, divTwo, divThree) {
+    //     return (
+    //         <section>
+    //             <h2>{conference} Conference</h2>
+    //             {getDivision(divOne)}
+    //             {getDivision(divTwo)}
+    //             {getDivision(divThree)}
+    //         </section>
+    //     )
+    // }
 
-    function getDivision(division) {
-        return (
-            <div>
-                <h4>{division} Division</h4>
-                {standingsData.filter(team => team.division.name === division.toLowerCase()).map((team, idx) => (
-                <span key={idx}>{team.team.name} {team.win.total} {team.loss.total}</span>
-                ))}
-            </div>
-        )
-    }
+    // function getDivision(division) {
+    //     return (
+    //         <div>
+    //             <h4>{division} Division</h4>
+    //             {standingsData.filter(team => team.division.name === division.toLowerCase()).map((team, idx) => (
+    //             <span key={idx}>{team.team.name} {team.win.total} {team.loss.total}</span>
+    //             ))}
+    //         </div>
+    //     )
+    // }
 
-    console.log(standingsData)
+    // console.log(standingsData)
 
     return (
-        isLoading ? <Spinner /> : 
+        // isLoading ? <Spinner /> : 
         <>
             <div>
                 <h1>Standings</h1>
-                {getConference('Eastern', 'Atlantic', 'Central', 'Southeast')}
-                {getConference('Western', 'Northwest', 'Pacific', 'Southwest')}
+                {/* {getConference('Eastern', 'Atlantic', 'Central', 'Southeast')} */}
+                {/* {getConference('Western', 'Northwest', 'Pacific', 'Southwest')} */}
             </div>
         </>
     )
