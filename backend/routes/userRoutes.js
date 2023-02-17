4441e249412c54fa03854de35ab53de0b51e59e7
HouliHeeler <luke_houlihan@hotmail.com>
2023-02-17 09:10:36 -0800

    push to heroku

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,7 +5,6 @@ const { protect } = require('../middleware/authMiddleware')
 
 router.post('/', registerUser)
 router.post('/login', loginUser)
-//Use protect to stop users from seeing site without logging in
 router.get('/me', protect, getUserData)
 
 module.exports = router
